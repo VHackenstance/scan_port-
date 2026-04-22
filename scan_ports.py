5bdf26e8ba17994992499593f7b7053cf446520b
Vhackenstance <hackenstance@proton.me>
2026-04-22 16:32:09 -0500

    Scan ports basic script

--- scan_ports.py
+++ scan_ports.py
@@ -1,2 +1,14 @@
 #/usr/bin/env python
+import socket
+
+def scan_ports(ip, ports):
+    open_ports = []
+    for port in ports:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(1)
+        result = sock.connect_ex((ip, port))
+        if result == 0:
+            open_ports.append(port)
+        sock.close()
+    return open_ports
 
